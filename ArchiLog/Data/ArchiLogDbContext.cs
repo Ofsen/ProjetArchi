e546f3d42a47ad5487567363882da7aeb6c75f57
Ofsen <ofsensmailbox@protonmail.com>
2022-10-28 09:45:20 +0200

    Ajoutez des fichiers projet.

---
+++ ArchiLog/Data/ArchiLogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ArchiLibrary.Data;
+using ArchiLibrary.Models;
+using ArchiLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArchiLog.Data
+{
+    public class ArchiLogDbContext : BaseDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"");
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
